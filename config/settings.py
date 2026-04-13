92ad7d70d50906d8ed650de5b7aeb67d7edd3120
flashyman261 <flashyman261@gmail.com>
2026-04-14 05:05:23 +0530

    feat: initialize Django project settings with REST framework, CORS, and JWT authentication configurations

--- config/settings.py
+++ config/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS=[
-    "frontend_website",
+    "https://youtube-bookmark-frontend.vercel.app",
 ]
 
 from datetime import timedelta
